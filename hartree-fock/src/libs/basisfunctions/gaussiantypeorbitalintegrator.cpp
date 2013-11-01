da8475a69b6cd7f2984e96e07bcec993ed055193
Svenn-Arne Dragly <s@dragly.com>
2013-11-01 18:03:53 +0100

    Comment cleaning.

--- hartree-fock/src/libs/basisfunctions/gaussiantypeorbitalintegrator.cpp
+++ hartree-fock/src/libs/basisfunctions/gaussiantypeorbitalintegrator.cpp
@@ -36,14 +36,6 @@ void GaussianTypeOrbitalIntegrator::reset() {
     rowvec PA = P - A;
     rowvec PB = P - B;
 
-//    cout << "p=" << p << endl;
-//    cout << "mu=" << mu << endl;
-//    cout << "AB=" << AB << endl;
-//    cout << "P=" << P << endl;
-
-//    cout << "PA = " << PA << endl;
-//    cout << "PB = " << PB << endl;
-
     m_totalExponent = p;
     m_reducedExponent = mu;
     m_relativeSeparation = AB;
@@ -53,91 +45,10 @@ void GaussianTypeOrbitalIntegrator::reset() {
 
     setupE();
     setupR();
-//    cout << m_R(0) << endl;
 }
 
 void GaussianTypeOrbitalIntegrator::setupE() {
-
-//    double p = m_totalExponent;
-//    double mu = m_reducedExponent;
-//    const rowvec &AB = m_relativeSeparation;
-//    const rowvec &C = m_corePositionC;
-//    const rowvec &P = m_centerOfMass;
-//    const rowvec &PC = P - C;
     m_E = HermiteExpansionCoefficient(m_exponentA, m_corePositionA, m_exponentB, m_corePositionB, m_angularMomentumAMax);
-//    int maxL = max(maxAngularMomentumA(), maxAngularMomentumB());
-//    for(int dim = 0; dim < 3; dim++) {
-//        int maxiA = maxL + 3;
-//        int maxiB = maxL + 3;
-//        // Since we are only going to need E_i_j_0 for the integrals, we cap t to i and j to only have the
-//        // needed values available for the algorithm.
-//        int maxt = 2 * maxL;
-//        m_E[dim] = zeros(maxiA, maxiB, maxt);
-
-//        double p = m_totalExponent;
-//        double mu = m_reducedExponent;
-//        double dim_AB = m_relativeSeparation(dim);
-//        double dim_PA = m_centerOfMassDiffA(dim);
-//        double dim_PB = m_centerOfMassDiffB(dim);
-
-//        //    vector<string> performed;
-
-//        // First row is special
-//        m_E[dim](0,0,0) = exp(-mu * dim_AB * dim_AB);
-////        cout << "KAB=" << m_E[dim](0,0,0) << endl;
-//        //    performed.push_back("000");
-//        for(int iB = 0; iB < maxiB; iB++) {
-//            for(int t = 0; t < maxt - 1; t++) {
-//                int iA = 0;
-//                if(iA == 0 && iB == 0 && t == 0) {
-//                    continue;
-//                }
-//                // p = previous, n = next
-//                // E(t,i,j) = 1 / (2*p) * E(t-1,i,j-1) + XPA * E(t,i,j-1) + (t + 1)*E(t+1,i,j-1)
-//                int iBp = iB - 1;
-//                int tp = t - 1;
-//                int tn = t + 1;
-//                double E_iA_iBp_tp = 0;
-//                if(checkIndexCombinationForE(iA, iBp, tp)) {
-//                    E_iA_iBp_tp = m_E[dim](iA, iBp, tp);
-//                }
-//                double E_iA_iBp_t = 0;
-//                if(checkIndexCombinationForE(iA, iBp, t)) {
-//                    E_iA_iBp_t = m_E[dim](iA, iBp, t);
-//                }
-//                double E_iA_iBp_tn = 0;
-//                if(checkIndexCombinationForE(iA, iBp, tn)) {
-//                    E_iA_iBp_tn = m_E[dim](iA, iBp, tn);
-//                }
-//                m_E[dim](iA,iB,t) = 1 / (2*p) * E_iA_iBp_tp + dim_PB * E_iA_iBp_t +  (t + 1)*E_iA_iBp_tn;
-//            }
-//        }
-//        for(int iA = 1; iA < maxiA; iA++) {
-//            for(int iB = 0; iB < maxiB; iB++) {
-//                for(int t = 0; t < maxt - 1; t++) {
-//                    // p = previous, n = next
-//                    // E(t,i,j) = 1 / (2*p) * E(t-1,i-1,j) + XPA * E(t,i-1,j) + (t + 1)*E(t+1,i-1,j)
-//                    int iAp = iA - 1;
-//                    int tp = t - 1;
-//                    int tn = t + 1;
-//                    double E_iAp_iB_tp = 0;
-//                    if(checkIndexCombinationForE(iAp, iB, tp)) {
-//                        E_iAp_iB_tp = m_E[dim](iAp, iB, tp);
-//                    }
-//                    double E_iAp_iB_t = 0;
-//                    if(checkIndexCombinationForE(iAp, iB, t)) {
-//                        E_iAp_iB_t = m_E[dim](iAp, iB, t);
-//                    }
-//                    double E_iAp_iB_tn = 0;
-//                    if(checkIndexCombinationForE(iAp, iB, tn)) {
-//                        E_iAp_iB_tn = m_E[dim](iAp, iB, tn);
-//                    }
-//                    m_E[dim](iA,iB,t) = 1 / (2*p) * E_iAp_iB_tp + dim_PA * E_iAp_iB_t +  (t + 1)*E_iAp_iB_tn;
-//                }
-//            }
-//        }
-////        cout << m_E[dim];
-//    }
 }
 
 double GaussianTypeOrbitalIntegrator::overlapIntegral(int dim, int iA, int iB)
@@ -183,10 +94,7 @@ double GaussianTypeOrbitalIntegrator::coreColoumbIntegral(int iA, int jA, int kA
 }
 
 double GaussianTypeOrbitalIntegrator::kineticIntegral(int dim, int iA, int iB) {
-//    double a = m_exponentA;
     double b = m_exponentB;
-//    double p = a + b;
-//    const cube &E_dim = m_E[dim];
     double S_iA_iBnn = overlapIntegral(dim, iA, iB + 2);
     double S_iA_iB = overlapIntegral(dim, iA, iB);
     double S_iA_iBpp;
@@ -212,16 +120,6 @@ double GaussianTypeOrbitalIntegrator::kineticIntegral(int iA, int jA, int kA, in
     return result; // TODO: Is there an error in the slides here?
 }
 
-//double GaussianTypeOrbitalIntegrator::boysFunction(double arg) {
-//    if (arg < 1.0E-6){
-//        return 1.0;
-//    } else {
-//        arg = sqrt(arg);
-//        double f = 1.0/arg * erf(arg) *sqrt(M_PI)/2.0;
-//        return f;
-//    }
-//}
-
 bool hasIndices(const vector<int>& neededIndices, const vector<vector<int>>& calculatedIndices) {
     bool found = false;
     for(vector<int> indices : calculatedIndices) {
@@ -240,29 +138,12 @@ bool hasIndices(const vector<int>& neededIndices, const vector<vector<int>>& cal
 }
 
 void GaussianTypeOrbitalIntegrator::setupR() {
-//    for(int dim = 0; dim < 3; dim++) {
-//        double p = m_totalExponent;
-//        double mu = m_reducedExponent;
-//        double dim_AB = m_relativeSeparation(dim);
-//        double dim_P = m_centerOfMass(dim);
-//        double dim_PA = m_centerOfMassDiffA(dim);
-//        double dim_PB = m_centerOfMassDiffB(dim);
-//        double R0000 = boysFunction();
-//    }
-//    for(int dim = 0; dim < 3; dim++) {
-//    double a = m_exponentA;
     double p = m_totalExponent;
-//    double mu = m_reducedExponent;
-//    const rowvec &AB = m_relativeSeparation;
     const rowvec &C = m_corePositionC;
     const rowvec &P = m_centerOfMass;
     const rowvec &PC = P - C;
     m_R = HermiteIntegral(p, PC, m_angularMomentumAMax);
 }
-
-//double GaussianTypeOrbitalIntegrator::coreIntegral() {
-
-//}
 
 rowvec GaussianTypeOrbitalIntegrator::corePositionB() const
 {
