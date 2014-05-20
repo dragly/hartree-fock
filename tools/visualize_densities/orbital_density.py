7fef3cfdcea3a64397d1a4be659c35c3bd9d9f13
Svenn-Arne Dragly <s@dragly.com>
2014-05-20 21:04:48 +0200

    Added orbital density script and updated density script

--- tools/visualize_densities/orbital_density.py
+++ tools/visualize_densities/orbital_density.py
@@ -4,6 +4,7 @@ from mayavi import mlab
 from os.path import join
 import h5py
 from argparse import ArgumentParser
+from glob import glob
 
 parser = ArgumentParser()
 parser.add_argument("results_path")
@@ -40,30 +41,32 @@ def draw_atoms(atoms, atom_meta):
         counter += 1
     return n_electrons
 
-density_file_name = os.path.join(args.results_path, "density.h5")
 
 atoms_data_file = h5py.File(join(args.results_path, "results.h5"))
 atom_meta = atoms_data_file.get("atomMeta")[:]
 atoms = atoms_data_file.get("state")[:]
 atoms_data_file.close()
 
-density_file = h5py.File(density_file_name)
-data = density_file.get("dataset")[:]
-density_file.close()
-
-print "Data min,max:",data.min(),data.max()
-
-X,Y,Z = mgrid[-3:3:1j*data.shape[0], -3:3:1j*data.shape[1], -3:3:1j*data.shape[2]]
-
-mlab.figure(2, bgcolor=(0, 0, 0), size=(1280, 720))
-mlab.clf()
-n_electrons = draw_atoms(atoms, atom_meta)
-data_max_min_diff = (data.max() - data.min())
-levels = [0.0003, 0.008]
-contours = []
-for level in levels:
-    contours.append(data.min() + level * data_max_min_diff)
-contours = [0.005, 0.05, 0.1]
-iso = mlab.contour3d(X, Y, Z, data, vmin=contours[0], vmax=contours[-1], opacity=0.5, contours=contours)
-
-mlab.savefig(os.path.join(output_dir, "density.x3d"))
+counter = 0
+for density_file_name in glob(os.path.join(args.results_path, "orbital_density_*.h5")):
+    density_file = h5py.File(density_file_name)
+    data = density_file.get("dataset")[:]
+    density_file.close()
+    
+    print "Data min,max:",data.min(),data.max()
+    
+    X,Y,Z = mgrid[-3:3:1j*data.shape[0], -3:3:1j*data.shape[1], -3:3:1j*data.shape[2]]
+    
+    mlab.figure(bgcolor=(0, 0, 0), size=(1280, 720))
+    mlab.clf()
+    n_electrons = draw_atoms(atoms, atom_meta)
+    data_max_min_diff = (data.max() - data.min())
+    levels = [0.0003, 0.008]
+    contours = []
+    for level in levels:
+        contours.append(data.min() + level * data_max_min_diff)
+    contours = [0.01]
+    iso = mlab.contour3d(X, Y, Z, data, vmin=contours[0], vmax=contours[-1], opacity=0.5, contours=contours)
+    
+    mlab.savefig(os.path.join(output_dir, "orbital_density_" + str(counter) + ".x3d"))
+    counter += 1
